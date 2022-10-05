c3f5099088e1eea235cbc2a4ea7487f4bafd94c8
Ian Laidlaw <ian@kaboodles.com>
2022-10-04 22:23:20 -0400

    Tiles added

--- src/tile.hpp
+++ src/tile.hpp
@@ -1,10 +1,58 @@
 #pragma once
 
-#include "entity.hpp"
+#include <optional>
+#include <utility>
+#include <vector>
+#include <unordered_map>
 
-struct Tile : GameObject{
-    //Feature/Buildable
-    //Item
-    //Entity
+#include "entity.hpp"
+#include "material.hpp"
+
+//TODO: actually make Features/Buildables
+//struct Feature{};
+//
+//TODO: actually make Items
+//struct Item{};
+
+class Tile : GameObject{
+    //std::optional<Feature> feature;
+    //std::vector<Item> items;
+    Entity* entity;
     //Fluid
+    Material material;
+   public:
+
+   public:
+    Tile() : entity(nullptr), material(Material::STONE){}
+    explicit Tile(const Material& material) : entity(nullptr), material(material) {}
+
+    void setEntity(Entity* newEntity){
+        entity = newEntity;
+    }
+
+    void removeEntity(){
+        entity = nullptr;
+    }
+
+    Entity* getEntity(){
+        return entity;
+    }
+
+//    void setMaterial(const Material newMaterial){
+//        material = newMaterial;
+//    }
+
+    [[nodiscard]] Material getMaterial() const{
+        return material;
+    }
+
+//    bool containsEntity() const;
+//
+//    void addFeature(Feature feature);
+//    void removeFeature(Feature feature);
+//    Feature getFeature() const;
+//
+//    void addItem(Item item);
+//    void removeItem(Item item);
+
 };
